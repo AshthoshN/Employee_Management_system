b083a708c463f8313334cd6485ecf88cd07becaa
AshthoshN <4al24cs038@aiet.org.in>
2026-05-14 20:17:05 +0530

    Update Employee.java

--- src/main/java/com/employee/model/Employee.java
+++ src/main/java/com/employee/model/Employee.java
@@ -6,8 +6,8 @@ public class Employee {
 
     private int empno;
     private String empName;
+    private String role;
     private Date doj;
-    private String gender;
     private double bsalary;
 
     public int getEmpno() { return empno; }
@@ -16,11 +16,11 @@ public class Employee {
     public String getEmpName() { return empName; }
     public void setEmpName(String empName) { this.empName = empName; }
 
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
     public Date getDoj() { return doj; }
     public void setDoj(Date doj) { this.doj = doj; }
-
-    public String getGender() { return gender; }
-    public void setGender(String gender) { this.gender = gender; }
 
     public double getBsalary() { return bsalary; }
     public void setBsalary(double bsalary) { this.bsalary = bsalary; }
